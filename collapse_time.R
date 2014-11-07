0130c41f57d68cb446700486c51e07366d4db47e
mike breecher <michaelbreecher@gmail.com>
2014-11-07 08:47:32 -0700

    changed logic to take hours between quarter end date and filing deadline, and reduced to a single for loop

--- collapse_time.R
+++ collapse_time.R
@@ -17,17 +17,16 @@ collapsed_history$billable_time <- NA
 collapsed_history$concurrent_services <- NA
 
 ptm <- proc.time()
-for (account in unique(collapsed_history$Account.Name)){
-  for(deadline in unique(collapsed_history[collapsed_history$Account.Name %in% account,]$filing.estimate)){
-    loop_timelog <- timelog[timelog$Account.Name %in% account & timelog$Billable %in% c("1") &
-                              timelog$Date >= deadline - 40 & timelog$Date <= deadline & !is.na(timelog$Date), ]
+uniques <- unique(collapsed_history[, c("Account.Name", "Quarter.End", "filing.estimate")])
+for (i in 1:nrow(uniques)){
+    loop_timelog <- timelog[timelog$Account.Name %in% uniques[i,1] & timelog$Billable %in% c("1") &
+                              timelog$Date >= uniques[i,2] & timelog$Date <= uniques[i,3] & !is.na(timelog$Date), ]
     if (!is.null(loop_timelog)){
-      collapsed_history[collapsed_history$Account.Name %in% account & 
-                          collapsed_history$filing.estimate %in% deadline,]$billable_time <- sum(loop_timelog$Hours)
-      collapsed_history[collapsed_history$Account.Name %in% account & 
-                          collapsed_history$filing.estimate %in% deadline,]$concurrent_services <- dim(loop_timelog)[1]
+      collapsed_history[collapsed_history$Account.Name %in% uniques[i,1] & 
+                          collapsed_history$filing.estimate %in% uniques[i,3],]$billable_time <- sum(loop_timelog$Hours)
+      collapsed_history[collapsed_history$Account.Name %in% uniques[i,1] & 
+                          collapsed_history$filing.estimate %in% uniques[i,3],]$concurrent_services <- length(unique(loop_timelog$Services.ID))
     }
-  }
 }
 proc.time() - ptm
 
