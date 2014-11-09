74f68928d9129192c82baa0abb8250572384a70c
mike breecher <michaelbreecher@gmail.com>
2014-11-09 14:16:43 -0700

    added export code to diy_periods.R. Also added calendar quarter to result dataframe so we can look at things relative to customer quarter and actual calendar quarter.

--- diy_periods.R
+++ diy_periods.R
@@ -16,7 +16,7 @@ collapsed_history <- merge(services, collapsed_time, "Services.ID", all.x = T)
 #for each unique account and logged date, use the distance from the year end date to assign a quarter bucket
 
 diy_bucketed_timelog <- ddply(timelog,
-                           .var = c("Account.Name", "Date"),
+                           .var = c("Account.Name", "Date", "Hours"),
                            .fun = function(x) {
                              
                              # Grab the year end from the services
@@ -28,13 +28,28 @@ diy_bucketed_timelog <- ddply(timelog,
                              qd <- as.numeric((unique(x$Date)-x_ye)/91)%%4 #quarter difference from year end
                              #if(abs(qd > 4)){qd <- qd%%4} #get a mod 4 quarter difference
                              cq <- ceiling(qd)
+                             aq <- paste("Q", ceiling(as.numeric(month(unique(x$Date))/3)), year(unique(x$Date)), sep = "")
                              
                              if(cq < 0){
-                               data.frame(timelog_quarter = paste("Q",cq, " ",year(unique(x$Date)), sep = ""), year_end = x_ye)  
+                               data.frame(customer_quarter = paste("Q",cq, " ",year(unique(x$Date)), sep = ""), 
+                                          calendar_quarter = aq,
+                                          year_end = x_ye)  
                              }else{
-                               data.frame(timelog_quarter = paste("Q",abs(cq), " ",year(unique(x$Date))+1, sep = ""), year_end = x_ye)
+                               data.frame(customer_quarter = paste("Q",abs(cq), " ",year(unique(x$Date))+1, sep = ""), 
+                                          calendar_quarter = aq,
+                                          year_end = x_ye)
                              }
                              
                              #browser()
                            }
 )
+
+diy_time <- with(diy_bucketed_timelog,
+                 aggregate(Hours ~ Account.Name + customer_quarter + calendar_quarter + year_end, FUN = sum))
+#order by account name then customer quarter
+diy_time <- diy_time[order(diy_time$Account.Name, diy_time$customer_quarter),]
+
+# code to export 
+setwd('C:/R/workspace/collapsed_time/output')
+export <- diy_time
+write.csv(export, file = "diy_time.csv", row.names = F, na = "")
