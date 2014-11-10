4704b6b466d05eb8aa295b0d6da5ae15c16a4b33
mike breecher <michaelbreecher@gmail.com>
2014-11-10 06:09:26 -0700

    added some na handling logic, and subsetted the timelog to include only billable hours

--- diy_periods.R
+++ diy_periods.R
@@ -16,28 +16,38 @@ collapsed_history <- merge(services, collapsed_time, "Services.ID", all.x = T)
 #for each unique account and logged date, use the distance from the year end date to assign a quarter bucket
 
 diy_bucketed_timelog <- ddply(timelog,
-                           .var = c("Account.Name", "Date", "Hours"),
+                           .var = c("Account.Name", "Date"),
                            .fun = function(x) {
                              
                              # Grab the year end from the services
                              x_services <- subset(services, subset = Account.Name %in% x$Account.Name, na.rm = T)
                              x_ye <- unique(x_services$Year.End)
-                             if (length(x_ye)<1){x_ye <- "12/31"}
-                             x_ye <- as.Date(paste(year(unique(x$Date)),x_ye, sep = "/"), format = "%Y/%m/%d")
-
-                             qd <- as.numeric((unique(x$Date)-x_ye)/91)%%4 #quarter difference from year end
-                             #if(abs(qd > 4)){qd <- qd%%4} #get a mod 4 quarter difference
-                             cq <- ceiling(qd)
-                             aq <- paste("Q", ceiling(as.numeric(month(unique(x$Date))/3)), year(unique(x$Date)), sep = "")
+                             if (!(is.null(x_ye))){
+                               x_ye <- as.Date(paste(year(unique(x$Date)),x_ye, sep = "/"), format = "%Y/%m/%d")
+  
+                               qd <- as.numeric((unique(x$Date)-x_ye)/91)%%4 #quarter difference from year end
+                               #if(abs(qd > 4)){qd <- qd%%4} #get a mod 4 quarter difference
+                               cq <- ceiling(qd)
+                               aq <- paste("Q", ceiling(as.numeric(month(unique(x$Date))/3)), year(unique(x$Date)), sep = "")
+                             }
                              
-                             if(cq < 0){
-                               data.frame(customer_quarter = paste("Q",cq, " ",year(unique(x$Date)), sep = ""), 
-                                          calendar_quarter = aq,
-                                          year_end = x_ye)  
+                             if(!is.na(cq)){
+                               if(cq < 0){
+                                 data.frame(customer_quarter = paste("Q",cq, " ",year(unique(x$Date)), sep = ""), 
+                                            calendar_quarter = aq,
+                                            year_end = x_ye,
+                                            Hours = sum(x$Hours))  
+                               }else{
+                                 data.frame(customer_quarter = paste("Q",abs(cq), " ",year(unique(x$Date))+1, sep = ""), 
+                                            calendar_quarter = aq,
+                                            year_end = x_ye,
+                                            Hours = sum(x$Hours))
+                               }
                              }else{
-                               data.frame(customer_quarter = paste("Q",abs(cq), " ",year(unique(x$Date))+1, sep = ""), 
-                                          calendar_quarter = aq,
-                                          year_end = x_ye)
+                               data.frame(customer_quarter = NA, 
+                                          calendar_quarter = NA,
+                                          year_end = NA,
+                                          Hours = sum(x$Hours))
                              }
                              
                              #browser()
