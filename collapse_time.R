eb685ac9fb93ccc91ae790a51767e377960faa56
mike breecher <michaelbreecher@gmail.com>
2014-11-18 16:30:45 -0700

    updated logic to assign customer quarter, and added it to bothproject and billable timelogs, so we can merge on that field.

--- collapse_time.R
+++ collapse_time.R
@@ -5,11 +5,12 @@ library(RecordLinkage)
 # Pull in import functions
 setwd("C:/R/workspace/shared")
 source("import_functions.R")
-
 setwd('C:/R/workspace/collapsed_time')
+source("diy_periods.R")
 
 services <- import_services()
 timelog <- import_timelog()
+diy_time <- import_billable() 
 
 collapsed_time <- aggregate(Hours ~ Services.ID, FUN = sum, data = timelog)
 
@@ -19,47 +20,56 @@ collapsed_history <- merge(services, collapsed_time, "Services.ID", all.x = T)
 pts <- proc.time()
 count.unique <- function(x) { length(unique(x[!is.na(x)])) } #function to count unique non-NA values
 
-# use ddply to aggregate
-collapsed_timelog <- ddply(collapsed_history,
-                           .var = c("Account.Name", "Quarter.End", "filing.estimate"),
-                           .fun = function(x) {
-                             
-#                              if (dim(x)[1] > 1){
-#                                browser()  
-#                              }
-                             # Grab the appropriate subset of timelog
-                             x_timelog <- subset(timelog, 
-                                                 subset = Account.Name %in% x$Account.Name &
-                                                   Billable %in% 1 &
-                                                   Date >= unique(x$Quarter.End) &
-                                                   Date <= unique(x$filing.estimate) &
-                                                   !is.na(Date) &
-                                                   !is.na(Hours)
-                             )
-                             
-                             # Aggregate using summarise - basically generates a data.frame with just
-                             # the variables I name on the 2nd and 3rd lines
-                             summarise(x_timelog,
-                                       billable_time = sum(Hours),
-                                       concurrent_services = count.unique(x_timelog$Services.ID),
-                                       divided_billable_time = sum(Hours)/count.unique(x_timelog$Services.ID)
-                             )
-                            }
+#I need customer period to merge diy time
+customer_period <- ddply(collapsed_history,
+                              .var = c("Account.Name", "filing.estimate", "Service.Name", "Service.Type", "Form.Type", "Quarter.End"),
+                              .fun = function(x) {
+                                
+                                # Grab the year end from the services
+                                x_ye <- unique(x$Year.End)
+                                x_ye <- as.Date(paste(year(unique(x$filing.estimate)),x_ye, sep = "/"), format = "%Y/%m/%d")
+                                
+                                qd <- as.numeric((unique(x$filing.estimate)-x_ye)/91)%%4 #quarter difference from year end
+                                #if(abs(qd > 4)){qd <- qd%%4} #get a mod 4 quarter difference
+                                if(!is.na(qd)){if(qd >= 0){cq <- ceiling(qd)}else{cq <- floor(qd)}}
+                                
+                                aq <- paste("Q", ceiling(as.numeric(month(unique(x$filing.estimate))/3)), year(unique(x$filing.estimate)), sep = "")
+                                
+                                if(!is.na(cq) & !(x_ye %in% c("     ")) & !is.na(x_ye)){
+                                  if(unique(x$filing.estimate) < x_ye){
+                                    data.frame(customer_quarter = paste("Q",cq, " ",year(unique(x$filing.estimate)), sep = ""), 
+                                               calendar_quarter = aq,
+                                               year_end = x_ye,
+                                               Hours = sum(x$Hours))  
+                                  }else{
+                                    data.frame(customer_quarter = paste("Q",abs(cq), " ",year(unique(x$filing.estimate))+1, sep = ""), 
+                                               calendar_quarter = aq,
+                                               year_end = x_ye,
+                                               Hours = sum(x$Hours))
+                                  }
+                                }else{
+                                  data.frame(customer_quarter = NA, 
+                                             calendar_quarter = aq,
+                                             year_end = NA,
+                                             Hours = sum(x$Hours))
+                                }
+                                
+                                #browser()
+                              }
 )
 
+diy_time_simple <- diy_time[,c("Account.Name", "customer_quarter", "calendar_quarter", "Hours")]
+names(diy_time_simple) <- c("Account.Name", "customer_quarter", "calendar_quarter", "Billable.Hours")
+# diy_time_simple$calendar_quarter <- paste(substr(diy_time_simple$calendar_quarter,3,6), 
+#                                           substr(diy_time_simple$calendar_quarter,2,2), sep = "")
+export <- merge(customer_period, diy_time_simple, by = c("Account.Name", "customer_quarter", "calendar_quarter"), all = T)
 
-# Then merging the aggregated results
-collapsed_history_time <- merge(x = collapsed_history,
-                                y = collapsed_timelog,
-                                by = c("Account.Name", "filing.estimate"),
-                                all.x = TRUE
-)
-proc.time() - pts
+#some cleanup
+names(export[c("Hours")]) <- c("Project.Hours")
+export[is.na(export$Service.Name),]$customer_quarter <- NA
 
 # code to export 
 setwd('C:/R/workspace/collapsed_time/output')
-export <- collapsed_history_time
-export$divided_billable_time[is.na(export$divided_billable_time)] <- NA
 export <- data.frame(lapply(export, as.character), stringsAsFactors = F)
 export[is.na(export)] <- ""
 write.csv(export, file = "CollapsedHistoryR.csv", row.names = F, na = "")
--- diy_periods.R
+++ diy_periods.R
@@ -3,72 +3,76 @@ library(plyr)
 library(RecordLinkage)
 
 # Pull in import functions
-setwd('C:/R/workspace/shared')
-source("import_functions.R")
-setwd('C:/R/workspace/collapsed_time')
 
-services <- import_services()
-timelog <- import_timelog()
-
-collapsed_time <- aggregate(Hours ~ Services.ID, FUN = sum, data = timelog)
-
-collapsed_history <- merge(services, collapsed_time, "Services.ID", all.x = T)
-
-timelog <- timelog[!is.na(timelog$Account.Name) & timelog$Billable %in% 1, ]
-
-#for each unique account and logged date, use the distance from the year end date to assign a quarter bucket
-diy_bucketed_timelog <- ddply(timelog,
-                           .var = c("Account.Name", "Date"),
-                           .fun = function(x) {
-                             
-                             # Grab the year end from the services
-                             x_services <- subset(services, subset = Account.Name %in% x$Account.Name, na.rm = T)
-                             x_ye <- unique(x_services$Year.End)
-                             x_ye <- as.Date(paste(year(unique(x$Date)),x_ye, sep = "/"), format = "%Y/%m/%d")
-
-                             qd <- as.numeric((unique(x$Date)-x_ye)/91)%%4 #quarter difference from year end
-                             #if(abs(qd > 4)){qd <- qd%%4} #get a mod 4 quarter difference
-                             cq <- ceiling(qd)
-                             aq <- paste("Q", ceiling(as.numeric(month(unique(x$Date))/3)), year(unique(x$Date)), sep = "")
-                             
-                             if(!is.na(cq)){
-                               if(cq < 0){
-                                 data.frame(customer_quarter = paste("Q",cq, " ",year(unique(x$Date)), sep = ""), 
-                                            calendar_quarter = aq,
-                                            year_end = x_ye,
-                                            Hours = sum(x$Hours))  
+import_billable <- function(){
+  setwd('C:/R/workspace/shared')
+  source("import_functions.R")
+  setwd('C:/R/workspace/collapsed_time')
+  
+  services <- import_services()
+  timelog <- import_timelog()
+  
+  collapsed_time <- aggregate(Hours ~ Services.ID, FUN = sum, data = timelog)
+  
+  collapsed_history <- merge(services, collapsed_time, "Services.ID", all.x = T)
+  
+  timelog <- timelog[!is.na(timelog$Account.Name) & timelog$Billable %in% 1, ]
+  
+  #for each unique account and logged date, use the distance from the year end date to assign a quarter bucket
+  diy_bucketed_timelog <- ddply(timelog,
+                             .var = c("Account.Name", "Date"),
+                             .fun = function(x) {
+                               
+                               # Grab the year end from the services
+                               x_services <- subset(services, subset = Account.Name %in% x$Account.Name, na.rm = T)
+                               x_ye <- unique(x_services$Year.End)
+                               x_ye <- as.Date(paste(year(unique(x$Date)),x_ye, sep = "/"), format = "%Y/%m/%d")
+  
+                               qd <- as.numeric((unique(x$Date)-x_ye)/91)%%4 #quarter difference from year end
+                               #if(abs(qd > 4)){qd <- qd%%4} #get a mod 4 quarter difference
+                               if(!is.na(qd)){if(qd >= 0){cq <- ceiling(qd)}else{cq <- floor(qd)}}
+                               aq <- paste("Q", ceiling(as.numeric(month(unique(x$Date))/3)), year(unique(x$Date)), sep = "")
+                               
+                               if(!is.na(cq)& !(x_ye %in% c("     ")) & !is.na(x_ye)){
+                                 if(unique(x$Date) < x_ye){
+                                   data.frame(customer_quarter = paste("Q",cq, " ",year(unique(x$Date)), sep = ""), 
+                                              calendar_quarter = aq,
+                                              year_end = x_ye,
+                                              Hours = sum(x$Hours))  
+                                 }else{
+                                   data.frame(customer_quarter = paste("Q",abs(cq), " ",year(unique(x$Date))+1, sep = ""), 
+                                              calendar_quarter = aq,
+                                              year_end = x_ye,
+                                              Hours = sum(x$Hours))
+                                 }
                                }else{
-                                 data.frame(customer_quarter = paste("Q",abs(cq), " ",year(unique(x$Date))+1, sep = ""), 
+                                 data.frame(customer_quarter = NA, 
                                             calendar_quarter = aq,
-                                            year_end = x_ye,
+                                            year_end = NA,
                                             Hours = sum(x$Hours))
                                }
-                             }else{
-                               data.frame(customer_quarter = NA, 
-                                          calendar_quarter = aq,
-                                          year_end = NA,
-                                          Hours = sum(x$Hours))
+                               
+                               #browser()
                              }
-                             
-                             #browser()
-                           }
-)
-
-#aggregate diy time by account and quarter
-diy_time <- with(diy_bucketed_timelog,
-                 aggregate(Hours ~ Account.Name + customer_quarter + calendar_quarter + year_end, FUN = sum))
-#customers with no services will be dropped from the above, so we need to grab them separately and bind the result.
-  diy_time_no_services <- with(diy_bucketed_timelog[is.na(diy_bucketed_timelog$customer_quarter),],
-                   aggregate(Hours ~ Account.Name, FUN = sum))
-  #add some columns and order before bind
-  diy_time_no_services$customer_quarter <- NA; diy_time_no_services$calendar_quarter <- NA; diy_time_no_services$year_end <- NA
-  diy_time_no_services <- diy_time_no_services[,names(diy_time)]
-  diy_time <- rbind(diy_time, diy_time_no_services)
-
-#order by account name then customer quarter
-diy_time <- diy_time[order(diy_time$Account.Name, diy_time$customer_quarter),]
-
-# code to export 
-setwd('C:/R/workspace/collapsed_time/output')
-export <- diy_time
-write.csv(export, file = "diy_time.csv", row.names = F, na = "")
+  )
+  
+  #aggregate diy time by account and quarter
+  diy_time <- with(diy_bucketed_timelog,
+                   aggregate(Hours ~ Account.Name + customer_quarter + calendar_quarter + year_end, FUN = sum))
+  #customers with no services will be dropped from the above, so we need to grab them separately and bind the result.
+    diy_time_no_services <- with(diy_bucketed_timelog[is.na(diy_bucketed_timelog$customer_quarter),],
+                     aggregate(Hours ~ Account.Name, FUN = sum))
+    #add some columns and order before bind
+    diy_time_no_services$customer_quarter <- NA; diy_time_no_services$calendar_quarter <- NA; diy_time_no_services$year_end <- NA
+    diy_time_no_services <- diy_time_no_services[,names(diy_time)]
+    diy_time <- rbind(diy_time, diy_time_no_services)
+  
+  #order by account name then customer quarter
+  diy_time <- diy_time[order(diy_time$Account.Name, diy_time$customer_quarter),]
+  
+  # code to export 
+  setwd('C:/R/workspace/collapsed_time/output')
+  write.csv(diy_time, file = "diy_time.csv", row.names = F, na = "")
+  
+  diy_time
+}
